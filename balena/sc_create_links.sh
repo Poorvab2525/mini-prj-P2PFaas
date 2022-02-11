129a41d8acd0f48dec1d2b2cffc43cc42abcb435
Gabriele Proietti Mattia <pm.gabriele@outlook.com>
2022-02-11 12:58:48 +0100

    Added stack-learner

--- balena/sc_create_links.sh
+++ balena/sc_create_links.sh
@@ -2,5 +2,6 @@
 # create hard links
 ln -sfv ../../stack-discovery stack-discovery
 ln -sfv ../../stack-scheduler stack-scheduler
+ln -sfv ../../stack-learner stack-learner
 ln -sfv ../../experiments/functions/pigo-openfaas/build/pigo-face-detector fn-pigo
 
